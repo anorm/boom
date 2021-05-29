df7f579fee3b3cee672ba2d92be789bc7f4bed31
Anders Norman <anders@norman.info>
2021-05-29 10:39:55 +0200

    Starting to read yaml

--- cmd/boom/main.go
+++ cmd/boom/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
 )
 
+type Entity struct {
+	Type        string
+	Name        string
+	Description string
+	Shape       string
+}
+
 func main() {
-    fmt.Println("Boom!")
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+
+	entity := Entity{}
+	err = decoder.Decode(&entity)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(entity)
+
+	fmt.Println("Boom!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/anorm/boom/v2
 
 go 1.15
+
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
